5e0c951dfe77b230f3f569cdc92744c61b93b6c0
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-24 00:28:45 +0330

    Add Ad's list and edit its constructor.

--- ad.py
+++ ad.py
@@ -3,12 +3,16 @@ from base_advertising import BaseAdvertising
 
 class Ad(BaseAdvertising):
 
+    list = []
+
     def __init__(self, object_id, title, img_url, link, advertiser):
         super().__init__(object_id)
         self.title = title
         self.img_url = img_url
         self.link = link
         self.advertiser = advertiser
+        self.list.append(self)
+        advertiser.add_ad(self)
 
     @staticmethod
     def describe_me():
