b0d768757469cdfa96c3ce6d4429c82912316ff7
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-24 00:19:23 +0330

    Add BaseAdvertising's variables and methods.

--- base_advertising.py
+++ base_advertising.py
@@ -1,4 +1,17 @@
 class BaseAdvertising:
 
+    clicks = 0
+    views = 0
+
     def __init__(self, object_id):
         self.object_id = object_id
+
+    @staticmethod
+    def describe_me():
+        return 'Something'
+
+    def inc_clicks(self):
+        self.clicks += 1
+
+    def inc_views(self):
+        self.views += 1
