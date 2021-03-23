c36a33ae502ee907db4d9950ddf8dd930cf2243b
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-24 00:22:57 +0330

    Add Advertiser's variable and methods.

--- advertiser.py
+++ advertiser.py
@@ -3,6 +3,20 @@ from base_advertising import BaseAdvertising
 
 class Advertiser(BaseAdvertising):
 
+    total_clicks = 0
+
     def __init__(self, object_id, name):
         super().__init__(object_id)
         self.name = name
+
+    @staticmethod
+    def help():
+        return 'Something'
+
+    @staticmethod
+    def describe_me():
+        return 'Something'
+
+    def inc_clicks(self):
+        super().inc_clicks()
+        self.total_clicks += 1
