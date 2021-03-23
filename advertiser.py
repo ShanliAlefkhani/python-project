488b9cabc3cadf94185c64f6c388a15862b6cec3
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-24 00:27:09 +0330

    Add Advertiser's lists and add_ad method.

--- advertiser.py
+++ advertiser.py
@@ -4,10 +4,13 @@ from base_advertising import BaseAdvertising
 class Advertiser(BaseAdvertising):
 
     total_clicks = 0
+    list = []
+    ads = []
 
     def __init__(self, object_id, name):
         super().__init__(object_id)
         self.name = name
+        self.list.append(self)
 
     @staticmethod
     def help():
@@ -20,3 +23,7 @@ class Advertiser(BaseAdvertising):
     def inc_clicks(self):
         super().inc_clicks()
         self.total_clicks += 1
+
+    def add_ad(self, ad):
+        if ad not in self.ads:
+            self.ads.append(ad)
